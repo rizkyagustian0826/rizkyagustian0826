fcc174700e82622426688150622e36c7b24e54e7
rizkyagustian0826 <rizkyagustian049@gmail.com>
2024-11-05 12:04:05 +0800

    Add files via upload

---
+++ modul 3/01-Variabel/Main.py
@@ -0,0 +1,23 @@
+# variabel adalah tempat menyimpan data
+
+# menaruh data / assigment data
+a = 10
+x = 5
+panjang = 1000
+
+# memanggil variabel
+print("Nilai a : ", a)
+print("Nilai x : ", x)
+print("Nilai Panjang : ", "Panjang")
+
+# penamaan variabel
+nilai__y = 15 # dengan underscor
+juta10 = 1000000000 # boleh dipakai
+nilaiAngka = 100 # modal camel
+
+# mengubah isi variabel
+print("Nilai a : ", a) # nilai a awal = 10
+a = 7
+print("Nilai x : ", x) # nilai a awal = 7
+
+print("Nilai x : ", x) # nilai awal = 5
