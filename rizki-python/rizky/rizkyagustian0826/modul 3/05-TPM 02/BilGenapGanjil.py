fc212139148bbe107c8c4483529f2b4eea53abc2
rizkyagustian0826 <rizkyagustian049@gmail.com>
2024-11-05 12:00:55 +0800

    Delete rizki-python directory

--- rizki-python/rizky/rizkyagustian0826/modul 3/05-TPM 02/BilGenapGanjil.py
+++
@@ -1,8 +0,0 @@
-bilangan = int(input("masukkan bilangan"))
-
-if bilangan % 2 == 0:
-    print (bilangan, "adalah bilangan genap")
-else:
-    print (bilangan, "adalah bilangan ganjil")
-
-# selesai
